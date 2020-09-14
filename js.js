cd52f05d6877c06a4e333666b6c0c19392d41c86
juannumo <juanmqri@gmail.com>
2020-09-13 23:31:57 -0500

    Diseño de la barra de navegación lateral para el proyecto VideoTube (Momento 1)

---
+++ js.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('body').bootstrapMaterialDesign();
+});
+
+
+const openMenu = document.querySelector('#show-menu');
+const hideMenuIcon = document.querySelector('#hide-menu');
+const sideMenu = document.querySelector('#nav-menu');
+
+openMenu.addEventListener('click', function() {
+    sideMenu.classList.add('active')
+});
+
+hideMenuIcon.addEventListener('click', function() {
+    sideMenu.classList.remove('active');
+});
